778724bd7e3c750c6105c1e070a33a177124b391
dawid-kulikowski <dawidkulikowski94@gmail.com>
2022-07-01 21:15:26 +0200

    Try Catch Finally

--- src/main/java/application/VehicleService.java
+++ src/main/java/application/VehicleService.java
@@ -1,6 +1,8 @@
 package application;
 
 import java.io.IOException;
+import java.util.IllegalFormatException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class VehicleService {
@@ -10,34 +12,43 @@ public class VehicleService {
         Vehicle bike = new Bike();
 
 
+        try {
+            System.out.println("Year of production:");
+            vehicle.setProductionYear(scanner.nextInt());
+        } catch (InputMismatchException e) {
+            System.out.println("error");
+        } finally {
+            System.out.println("Code");
+        }
 
-        System.out.println("Brand: ");
-        vehicle.setBrand(scanner.next());
-        System.out.println("Model: ");
-        vehicle.setModel(scanner.next());
+
+
+
+//        System.out.println("Brand: ");
+//        vehicle.setBrand(scanner.next());
+//        System.out.println("Model: ");
+//        vehicle.setModel(scanner.next());
         System.out.println("Year of production:");
         vehicle.setProductionYear(scanner.nextInt());
-        System.out.println("Condition of Vehicle (NEW,USED) : ");
-        vehicle.setVehicleCondition(scanner.next());
-        System.out.println("Condition of Vehicle (NEW,USED) : ");
-        vehicle.setVehicleWorth(scanner.nextInt());
-
-        System.out.println("Brand: ");
-        bike.setBrand(scanner.next());
-        System.out.println("Model: ");
-        bike.setModel(scanner.next());
-        System.out.println("Year of production:");
-        bike.setProductionYear(scanner.nextInt());
-        System.out.println("Condition of Bike (NEW,USED) : ");
-        bike.setVehicleCondition(scanner.next());
-        System.out.println("Bike Estimated Worth: ");
-        bike.setVehicleWorth(scanner.nextInt());
-
-
-
-        System.out.println(vehicle.getBrand() + vehicle.getModel() + vehicle.getVehicleCondition());
-        vehicle.insuranceCost();
-        bike.insuranceCost();
+//        System.out.println("Condition of Vehicle (NEW,USED) : ");
+//        vehicle.setVehicleCondition(scanner.next());
+//        System.out.println("Condition of Vehicle (NEW,USED) : ");
+//        vehicle.setVehicleWorth(scanner.nextInt());
+//
+//        System.out.println("Brand: ");
+//        bike.setBrand(scanner.next());
+//        System.out.println("Model: ");
+//        bike.setModel(scanner.next());
+//        System.out.println("Year of production:");
+//        bike.setProductionYear(scanner.nextInt());
+//        System.out.println("Condition of Bike (NEW,USED) : ");
+//        bike.setVehicleCondition(scanner.next());
+//        System.out.println("Bike Estimated Worth: ");
+//        bike.setVehicleWorth(scanner.nextInt());
+//
+//        System.out.println(vehicle.getBrand() + vehicle.getModel() + vehicle.getVehicleCondition());
+//        vehicle.insuranceCost();
+//        bike.insuranceCost();
 
     }
 
